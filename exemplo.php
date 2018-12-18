35f4074d2c0ff253a16a61131680d84bf5010b29
Lenodeoliveira <lenodeoliveira@gmail.com>
2018-12-18 20:56:19 -0200

    calculadora php

--- exemplo.php
+++ exemplo.php
@@ -1,5 +1,54 @@
 <?php
+        # classe :: Calculadora
+        class Calculadora {
 
-echo "Olá mundo!";
+            # Função "Calcular" para executar o cálculo dos valores (v1 e v2)
+            public function Calcular() {
 
-?>
+                # Se for setado algum valor ào submit (doCalc), executa a operação
+                if (isset($_POST['doCalc'])) {
+                     
+                                       
+                    # Se a operação for soma (value = soma), então...
+                    if ($_POST['operacao'] == "soma") {
+
+                        # Armazena a soma de [v1 + v2] na variável $resultado
+                        $resultado = $_POST['v1'] + $_POST['v2'];
+
+                        # Exibe a variável $resultado com os valores já somados
+                        return $resultado;
+
+                        # Ou então, se a operação não for (value = soma), e sim (value = subtrai) então...
+                    } elseif ($_POST['operacao'] == "subtrai") {
+                        $resultado = $_POST['v1'] - $_POST['v2'];
+                        
+                        # exibe o resutado
+                        return $resultado;
+                    
+                    } elseif ($_POST['operacao'] == 'multiplica') {
+                        
+                        $resultado = $_POST['v1'] * $_POST['v2'];
+                        
+                        #exibe o resultado
+                        return $resultado;
+                    
+                    } elseif ($_POST['operacao'] == 'divide') {
+                        
+                        $resultado = $_POST['v1'] / $_POST['v2'];
+                        
+                        #exibe o resultado
+                        return $resultado;
+                    
+                    }
+                }
+            }
+
+        }
+              
+              
+
+        # Instancia a classe CALCULADORA()
+        $calcular = new Calculadora();
+            #FIM   
+ 
+        ?>

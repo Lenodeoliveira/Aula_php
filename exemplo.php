93b37d55841090c1a4b16df8cde62732a254421d
Lenodeoliveira <lenodeoliveira@gmail.com>
2018-12-18 21:16:50 -0200

    atualização

--- exemplo.php
+++ exemplo.php
@@ -1,3 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculo</title>
+       
+
+         <H1><strong>Calculadora php:</strong></H1>  
+      
+    </head>
+  <body>
+   
+ <form method="post" action="">
+           
+            <!--input que receberá o primeiro valor a ser calculado --> 
+
+            <input type="text" name="v1" placeholder="Valor 1" />
+
+            <!-- Select com o tipo de operação (Somar, Diminuir, Multiplicar ou Dividir -->
+            <select name="operacao">
+                <option value="soma">+</option>
+                <option value="subtrai">-</option>
+                <option value="multiplica">*</option>
+                <option value="divide">/</option>
+            </select>
+
+            <!-- Input que receberá o segundo valor a ser calculado -->
+            <input type="text" name="v2" placeholder="Valor 2" />
+
+            <!-- Input que enviará os valores para a função de cálculo -->
+            <input type="submit" name="doCalc" value="Calcular" /><br><br><br>
+
+
+        </form>
+
+  </body>
+ </html>
+
+
 <?php
         # classe :: Calculadora
         class Calculadora {
@@ -49,6 +90,11 @@
 
         # Instancia a classe CALCULADORA()
         $calcular = new Calculadora();
-            #FIM   
+        
+
+        
+        # Executa a função
+        echo $calcular->Calcular();
+        #FIM   
  
         ?>
